43c994fdef4be257893700cff2075274832fba62
Mazen Zwin <64337759+Zwin-ux@users.noreply.github.com>
2025-11-02 16:43:48 -0800

    fix: restore root layout for app router (#14)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,16 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
-          <LanguageProvider>
-            <LearningSessionProvider>
-              <XPProvider>
-                {children}
-                <Toaster position="top-center" richColors />
-              </XPProvider>
-            </LearningSessionProvider>
-          </LanguageProvider>
-        </ThemeProvider>
+        <RootProviders>{children}</RootProviders>
       </body>
     </html>
   )
